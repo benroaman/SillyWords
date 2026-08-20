9634c3278cfcdec9f271182922c0ecf3eff5ffdb
Ben Roaman <benroaman@MacBookPro.lan>
2026-08-20 16:15:41 -0400

    did a lot of refactoring to begin introducing better seperation of concerns and testability, in word generation tab to start

---
+++ SillyWords/UI/Screens/Shared/BadRouteView.swift
@@ -0,0 +1,31 @@
+//
+//  BadRouteView.swift
+//  SillyWords
+//
+//  Created by Ben Roaman on 8/20/26.
+//
+
+import SwiftUI
+
+struct BadRouteView: View {
+    let route: MainRoute
+    @State var didReport: Bool = false
+    
+    var body: some View {
+        VStack {
+            Text("Whoops!")
+                .font(.largeTitle)
+            Spacer().frame(height: 12)
+            Text("This route is not wired up correctly. A report has been sent to the developer. Thank you for your patience!")
+                .multilineTextAlignment(.center)
+        }.onAppear() {
+            guard !didReport else { return }
+            Telemetry.reportUnsupportedMainRoute(route)
+            didReport = true
+        }
+    }
+}
+
+#Preview {
+    BadRouteView(route: .settingsWordGeneration)
+}
