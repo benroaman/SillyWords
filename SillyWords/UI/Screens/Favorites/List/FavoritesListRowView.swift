d3235ddd1d5b9e3c767f74e6a152be40fb975c0e
Ben Roaman <benroaman@MacBookPro.lan>
2026-08-21 14:18:18 -0400

    refactored favorites model so view uses generic and model has proper test implementations

---
+++ SillyWords/UI/Screens/Favorites/List/FavoritesListRowView.swift
@@ -0,0 +1,104 @@
+//
+//  FavoritesListRowView.swift
+//  SillyWords
+//
+//  Created by Ben Roaman on 5/6/26.
+//
+
+import SwiftUI
+
+struct FavoritesListRowView<M: FavoritesListRowViewModel>: View {
+    // MARK: Instance Variables - State
+    @State var model: M
+    
+    // MARK: Instance Constants
+    let favorite: Favorite
+    
+    // MARK: Body
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                if let word = favorite.word {
+                    ZStack(alignment: .leading) {
+                        // Text("REF") helps Text(favorite.word) take a uniform amount of space as it scales
+                        Text("REF")
+                            .font(.largeTitle)
+                            .foregroundStyle(.clear)
+                            .frame(maxWidth: .infinity)
+                        Text(word)
+                            .font(.largeTitle)
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.5)
+                    }
+                }
+                if let date = favorite.dateAdded {
+                    Text(date.formatted(date: .long, time: .shortened))
+                        .font(.subheadline)
+                        .foregroundStyle(.secondary)
+                }
+            }
+            Spacer()
+        }
+        .swipeActions(allowsFullSwipe: false) {
+            makeRowSwipeActions(for: favorite)
+        }
+    }
+}
+
+// MARK: Private API - View Builders
+private extension FavoritesListRowView {
+    @ViewBuilder func makeRowSwipeActions(for favorite: Favorite) -> some View {
+        Button(action: {
+            model.onFavoriteListRowDeleteTap(for: favorite)
+        }, label: {
+            Image(Self.deleteActionIcon)
+        })
+        .tint(Style.Color.deleteTheme)
+        Menu(Self.reportMenuIcon) {
+            Button(action: {
+                model.onFavoriteListRowReportPoorQualityTap(for: favorite)
+            }, label: {
+                Label(title: Self.reportPoorQualityActionTitle, symbol: Self.reportActionIcon)
+            })
+            Button(action: {
+                model.onFavoriteListRowReportOffensiveTap(for: favorite)
+            }, label: {
+                Label(title: Self.reportOffensiveActionTitle, symbol: Self.reportActionIcon)
+            })
+            .tint(Style.Color.offensiveTheme)
+        }
+        .tint(Style.Color.reportTheme)
+    }
+}
+
+// MARK: Copy
+extension FavoritesListRowView {
+    static var reportPoorQualityActionTitle: String { "Poor Quality" }
+    static var reportOffensiveActionTitle: String { "Offensive" }
+    
+    static var deleteActionIcon: SFSymbol { .trash }
+    static var reportMenuIcon: SFSymbol { .exclamationmarkBubble }
+    static var reportActionIcon: SFSymbol { .envelope }
+}
+
+// MARK: Model Requirements
+protocol FavoritesListRowViewModel {
+    func onFavoriteListRowDeleteTap(for favorite: Favorite)
+    func onFavoriteListRowReportPoorQualityTap(for favorite: Favorite)
+    func onFavoriteListRowReportOffensiveTap(for favorite: Favorite)
+}
+
+// MARK: Previews
+#Preview {
+    PreviewWrapper()
+}
+
+fileprivate struct PreviewWrapper: View {
+    @State private var model = FavoritesListViewModelPreview()
+    
+    var body: some View {
+        List {
+            FavoritesListRowView(model: model, favorite: model.someFavorite)
+        }
+    }
+}
---
+++ SillyWords/UI/Screens/Favorites/List/FavoritesListView.swift
@@ -0,0 +1,116 @@
+//
+//  FavoritesListView.swift
+//  SillyWords
+//
+//  Created by Ben Roaman on 5/3/26.
+//
+
+import SwiftUI
+
+// MARK: Base
+struct FavoritesListView<M: FavoritesListViewModel>: View {
+    // MARK: Instance Variables - State
+    @FetchRequest private var favorites: FetchedResults<Favorite>
+    @State var model: M
+    
+    init(model: M) {
+        self.model = model
+        self._favorites = FetchRequest<Favorite>(
+            sortDescriptors: model.sort.favoriteSortDescriptors,
+            animation: .default
+        )
+    }
+    
+    // MARK: Body
+    var body: some View {
+        VStack {
+            if model.hasFavorites {
+                favoritesList
+            } else {
+                zeroItemView
+            }
+        }
+        .alert(model.deleteConfirmationMessage,
+               isPresented: model.isPresentingDeleteConfirmation,
+               actions: makeDeleteConfirmationAlertActions)
+    }
+}
+
+// MARK: Private API - View Builders
+private extension FavoritesListView {
+    @ViewBuilder var zeroItemView: some View {
+        zeroItemMessage
+            .font(Self.zeroItemMessageFont)
+            .foregroundStyle(Style.Color.zeroItemTheme)
+            .transition(.opacity.animation(.default))
+    }
+    
+    @ViewBuilder var favoritesList: some View {
+        List {
+            ForEach(favorites, id: \.self) { entry in
+                FavoritesListRowView(model: model, favorite: entry)
+            }
+        }
+        .onChange(of: model.sort, {_, _ in
+            favorites.sortDescriptors = model.sort.favoriteSortDescriptors
+        })
+        .safeAreaBar(edge: .top, alignment: .trailing) {
+            sortButton
+        }
+        .transition(.opacity.animation(.default))
+    }
+    
+    @ViewBuilder var sortButton: some View {
+        Picker(selection: $model.sort, content: {
+            ForEach(SortMode.allCases, id: \.self) { mode in
+                Image(mode.icon)
+                .tag(mode)
+            }
+        }, label: {
+            Image(model.sort.icon)
+                .contentTransition(.symbolEffect(.replace))
+        })
+        .labelsHidden()
+        .glassEffect(.clear)
+        .padding(.trailing)
+    }
+    
+    @ViewBuilder func makeDeleteConfirmationAlertActions() -> some View {
+        Button(Self.deleteConfirmationAlertConfirmActionTitle,
+               role: .destructive,
+               action: model.onFavoriteListConfirmDeleteTapped)
+        Button(Self.deleteConfirmationAlertCancelActionTitle,
+               role: .cancel,
+               action: { })
+    }
+}
+
+// MARK: Copy
+extension FavoritesListView {
+    // Instance
+    private var zeroItemMessage: Text { Text("Add favorites in the \(Image(systemName: "bubble.fill")) tab") }
+    
+    // Static
+    static var deleteConfirmationAlertConfirmActionTitle: String { "Delete" }
+    static var deleteConfirmationAlertCancelActionTitle: String { "Cancel" }
+}
+
+// MARK: Style
+extension FavoritesListView {
+    // Fonts
+    static var zeroItemMessageFont: Font { .headline }
+}
+
+// MARK: Previews
+#Preview {
+    PreviewWrapper()
+}
+
+fileprivate struct PreviewWrapper: View {
+    @State private var database = Database.preview
+    
+    var body: some View {
+        FavoritesListView(model: FavoritesListviewModelProd(manager: FavoritesManager(database), navigation: FavoritesTabNavigationPreview()))
+            .environment(\.managedObjectContext, database.viewContext)
+    }
+}
